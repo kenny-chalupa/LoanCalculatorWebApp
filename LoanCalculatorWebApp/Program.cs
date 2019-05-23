3b7179d0b2f72c6a1755f0fda1692895f3c95331
Kenny Chalupa <kechalup@microsoft.com>
2019-05-23 16:37:59 -0500

    Addressed FXCOP Warnings

--- LoanCalculatorWebApp/Program.cs
+++ LoanCalculatorWebApp/Program.cs
@@ -11,7 +11,7 @@ using System.Data.Common;
 
 namespace LoanCalculatorWebApp
 {
-    public class Program
+    public static class Program
     {
         public static void Main(string[] args)
         {
